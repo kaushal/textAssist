8723fbda0b44f85e2d72b85a55cb6c4b3f5897f1
kaushalp88 <kaushalp88@gmail.com>
2013-06-20 04:45:46 +0000

    add user works

--- restFlask.py
+++ restFlask.py
@@ -4,17 +4,27 @@ import json
 
 app = Flask(__name__)
 connection = Connection('localhost', 27017)
-db = connection.ufo
+db = connection.users
 
 @app.route('/user', methods=['GET', 'POST'])
 def user():
+    phoneNumber = 9082082212
+    name = 'kaushal'
+    group = 'test'
     if request.method == 'POST':
         if request.headers['Content-Type'] == 'application/json':
-            print db.collection_names()
-            print json.loads(request.data)
+            payload = json.loads(request.data)
+            print payload
+            try:
+                number = payload['number']
+                name = payload['name']
+                group = payload['group']
+                db.users.insert({"_id":number, "name": name, "group": group})
+                return 'success'
+            except KeyError:
+                return 'Error: invalid json for requested path: /user'
         else:
-            print 'failed'
-        return 'post'
+            return 'Error: data entered is not valid json'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
