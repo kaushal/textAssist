29fa707fd0d0bf53fdfeaf9c248b73ee233e4072
Kaushal Parikh <kaushalp88@gmail.com>
2013-07-20 01:37:55 -0400

    login might work now

--- textApp/www/js/parseMagic.js
+++ textApp/www/js/parseMagic.js
@@ -1,85 +1,97 @@
 window.magic = (function ($, document, window, Parse) {
-	"use strict";
+    "use strict";
 
-	var magic = {};
+    var magic = {};
 
-	//	Setup parse
-	var parseAppId = "bGwXAzYa23yIkCfbpHMM6j2CD9iBfvJ1YBdVt9V5";
-	var parseJavascriptId = "DrmyAyt0zDPOlcraBEGBs6wcgOJvQtfCf3DC67OW";
-	Parse.initialize(parseAppId, parseJavascriptId);
+    //	Setup parse
+    var parseAppId = "bGwXAzYa23yIkCfbpHMM6j2CD9iBfvJ1YBdVt9V5";
+    var parseJavascriptId = "DrmyAyt0zDPOlcraBEGBs6wcgOJvQtfCf3DC67OW";
+    Parse.initialize(parseAppId, parseJavascriptId);
 
-	//	Verify login with parse
-	magic.parseLogin = function(myNumber, myName) {
-		//	Returns a promise with success or failure
-		var promise;
-		return promise;
-	};
+    //	Verify login with parse
+    magic.parseLogin = function(myNumber, myName) {
+        //	Returns a promise with success or failure
+        var userTable = Parse.Object.extend("Users");
+        var query = new Parse.Query(userTable);
+
+        query.get(myNumber, {
+            success: function(userTable){
+                     }, 
+            error: function(userTable){
+                       var newUser = new userTable();
+                       newUser.set("objectId", myNumber);
+                       newUser.set("name", name);
+                   }
+
+        });
+        var promise;
+        return promise;
+    };
+
+    /*
+     *	Get my groups
+     *	Returns: Array(	Array(groupName, groupId) , ... )
+     */
+    magic.getMyGroups = function(myNumber) {
+
+    };
 
 
-	/*
-	*	Get my groups
-	*	Returns: Array(	Array(groupName, groupId) , ... )
-	*/
-	magic.getMyGroups = function(myNumber) {
+    /*
+     *	Get all of my friends who use the app
+     *	Takes in:	List of all the numbers in my contact list
+     *	Returns:	List of my friends numbers who use the app - Array( number1, number2, ...)
+     */
+    magic.getMyFriends = function(listOfMyFriendsNumbers) {
 
-	};
+    };
 
+    /*
+     *	Binds together the group name, owners number,
+     *		list of friends ids in the group, targets number, 
+     *		and the owners twilio number for this group
+     *	Returns:	The group Id for this group
+     */
+    magic.bindGroup = function(groupName, myNumber, friendIds, targetNumber) {
+        var twilioNumber = magic.getATwilioNumber();
 
-	/*
-	*	Get all of my friends who use the app
-	*	Takes in:	List of all the numbers in my contact list
-	*	Returns:	List of my friends numbers who use the app - Array( number1, number2, ...)
-	*/
-	magic.getMyFriends = function(listOfMyFriendsNumbers) {
+        var groupId;
+        return groupId;
+    };
 
-	};
+    /*
+     *	Creates or gets a new twilio number
+     *	Returns the twilio number to use
+     */
+    magic.getATwilioNumber = function() {
 
-	/*
-	*	Binds together the group name, owners number,
-	*		list of friends ids in the group, targets number, 
-	*		and the owners twilio number for this group
-	*	Returns:	The group Id for this group
-	*/
-	magic.bindGroup = function(groupName, myNumber, friendIds, targetNumber) {
-		var twilioNumber = magic.getATwilioNumber();
+    };
 
-		var groupId;
-		return groupId;
-	};
+    /*
+     *	Sends a message to the group or target
+     *	If target: Use the twilio number to message target, then send message to group
+     *	If group: Only send message to the group
+     *
+     *	string sendType: "group"/"target"
+     *	Return: true/false on success	
+     */
+    magic.sendMessageTo = function(sendType, message, groupId) {
+        if(sendType === 'target') {
+            //	Send message to target
+            //	Code here
 
-	/*
-	*	Creates or gets a new twilio number
-	*	Returns the twilio number to use
-	*/
-	magic.getATwilioNumber = function() {
+            //	then send message to group...
+            return magic.sendMessageTo('group', message, groupId);
+        }
+        else {
+            //	Send message to group 
+            //	Code here
 
-	};
+            return true;
+        }
+        return false;
+    };
 
-	/*
-	*	Sends a message to the group or target
-	*	If target: Use the twilio number to message target, then send message to group
-	*	If group: Only send message to the group
-	*
-	*	string sendType: "group"/"target"
-	*	Return: true/false on success	
-	*/
-	magic.sendMessageTo = function(sendType, message, groupId) {
-		if(sendType === 'target') {
-			//	Send message to target
-			//	Code here
-
-			//	then send message to group...
-			return magic.sendMessageTo('group', message, groupId);
-		}
-		else {
-			//	Send message to group 
-			//	Code here
-
-			return true;
-		}
-		return false;
-	};
-
-	return magic;
+    return magic;
 
 }(window.$, document, window, window.Parse));
