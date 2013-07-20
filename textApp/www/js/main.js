9762a0e1df06f1078fb5906756bd6b9059afe4bc
Carlos Gil <cgil1210@gmail.com>
2013-07-19 22:17:49 -0400

    templating parse fucntion

--- textApp/www/js/main.js
+++ textApp/www/js/main.js
@@ -4,13 +4,23 @@
 	$(function() {
 		var appId = "bGwXAzYa23yIkCfbpHMM6j2CD9iBfvJ1YBdVt9V5";
 		var javascriptId = "DrmyAyt0zDPOlcraBEGBs6wcgOJvQtfCf3DC67OW";
+
+		var ERROR_LOGIN = "Could not log in";
 		Parse.initialize(appId, javascriptId);
 
 
 		var login = function() {
 			var number = getMyNumber();
 			var name = getMyName();
-			var loginStatus = parseLogin(number, name);
+			var loginStatus = Number(parseLogin(number, name));
+			if(loginStatus === 1) {		//	Success logging in
+				goToLoginScreen();
+				return;
+			}							//	Failed to log in
+			else {					
+				displayError(ERROR_LOGIN);
+				return;
+			}
 		};
 
 		//	Queries phone to get my phone number
@@ -21,6 +31,16 @@
 		//	Gets the inputed user name from login screen
 		var getMyName = function() {
 			return $('#i-name').val();
+		};
+
+		//	Display error message
+		var displayError = function(error) {
+			$('#feedback').text(error);
+		};
+
+		//	Go to the login screen
+		var goToLoginScreen = function() {
+			
 		};
 
 
--- textApp/www/js/parseMagic.js
+++ textApp/www/js/parseMagic.js
@@ -3,15 +3,76 @@
 
 	$(function() {
 		//	Verify login with parse
-		var parseLogin = function(number, name) {
+		var parseLogin = function(myNumber, myName) {
 			//	Returns a promise with success or failure
 			var promise;
 			return promise;
 		};
 
-		
-		
 
+		/*
+		*	Get my groups
+		*	Returns: Array(	Array(groupName, groupId) , ... )
+		*/
+		var getMyGroups = function(myNumber) {
+
+		};
+
+
+		/*
+		*	Get all of my friends who use the app
+		*	Takes in:	List of all the numbers in my contact list
+		*	Returns:	List of my friends numbers who use the app - Array( number1, number2, ...)
+		*/
+		var getMyFriends = function(listOfMyFriendsNumbers) {
+
+		};
+
+		/*
+		*	Binds together the group name, owners number,
+		*		list of friends ids in the group, targets number, 
+		*		and the owners twilio number for this group
+		*	Returns:	The group Id for this group
+		*/
+		var bindGroup = function(groupName, myNumber, friendIds, targetNumber) {
+			var twilioNumber = getATwilioNumber();
+
+			var groupId;
+			return groupId;
+		};
+
+		/*
+		*	Creates or gets a new twilio number
+		*	Returns the twilio number to use
+		*/
+		var getATwilioNumber = function() {
+
+		};
+
+		/*
+		*	Sends a message to the group or target
+		*	If target: Use the twilio number to message target, then send message to group
+		*	If group: Only send message to the group
+		*
+		*	string sendType: "group"/"target"
+		*	Return: true/false on success	
+		*/
+		var sendMessageTo = function(sendType, message, groupId) {
+			if(sendType === 'target') {
+				//	Send message to target
+				//	Code here
+
+				//	then send message to group...
+				return sendMessageTo('group', message, groupId);
+			}
+			else {
+				//	Send message to group 
+				//	Code here
+
+				return true;
+			}
+			return false;
+		};
 
 	});
 
